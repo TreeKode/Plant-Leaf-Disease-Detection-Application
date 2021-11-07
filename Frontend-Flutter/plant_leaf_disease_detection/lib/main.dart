0f48313ba755d0a7249d88ad7364dec56cd729d2
Dhruv Lawaniya <dhruvlawaniya2510@gmail.com>
2021-11-07 21:31:00 +0530

    Big changes-Frontend
    Added Assets AAnd Changed UI

--- Frontend-Flutter/plant_leaf_disease_detection/lib/main.dart
+++ Frontend-Flutter/plant_leaf_disease_detection/lib/main.dart
@@ -1,46 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar:
-            PreferredSize(
-              preferredSize: Size.fromHeight(80.0),
-              child: AppBar(
-                  title: Center(child: Text('TreeKode')),
-                  backgroundColor : Colors.green[900],
-                  // leading: Icon(Icons.menu),
-
-              ),
-            ),
-        bottomNavigationBar: BottomNavigationBar(
-         type: BottomNavigationBarType.fixed,
-          onTap: (value){
-           //respond
-          },
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.account_box_sharp),
-              label: 'Profile',
-            ) ,
-            BottomNavigationBarItem(
-                icon: Icon(Icons.menu),
-              label: 'Menu',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark),
-              label: 'Search',
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
+  runApp(const BigApp());
 }
+class BigApp extends StatelessWidget {
+  const BigApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal[100],
+        appBar: AppBar(
+          title: Text(
+              'TreeKode'
+          ),
+          titleTextStyle: (
+          TextStyle(
+              fontFamily: 'Great_Vibes',
+            fontSize: 50,
+          )
+          ),
+          backgroundColor: Colors.teal,
+        ),
+      )
+    );
+  }
+}
+
+
+
 
